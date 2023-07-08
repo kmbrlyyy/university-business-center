90bf76b19bdbb52336c6558d8e929d758e0a7f6a
Kimberley Delgado <kimberleyvdelgado@gmail.com>
2023-07-08 20:36:39 +0800

    created node server

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'TUP Shirt',
+      slug: 'tup-shirt',
+      category: 'Shirt',
+      image: '/images/p1.jpg',
+      price: '120',
+      countInStock: '10',
+      description: 'Available in RED and WHITE.',
+    },
+    {
+      name: 'TUP Lanyard',
+      slug: 'tup-lanyard',
+      category: 'Lanyard',
+      image: '/images/p2.jpg',
+      price: '100',
+      countInStock: '10',
+      description: 'Reversible with Rubber Patch.',
+    },
+    {
+      name: 'TUP Bag',
+      slug: 'tup-bag',
+      category: 'Bag',
+      image: '/images/p3.jpg',
+      price: '120',
+      countInStock: '10',
+      description: 'Available in RED and WHITE.',
+    },
+    {
+      name: 'TUP Pin',
+      slug: 'tup-pin',
+      category: 'Pin',
+      image: '/images/p4.jpg',
+      price: '120',
+      countInStock: '10',
+      description: 'TUP College of Science Pin.',
+    },
+  ],
+};
+
+export default data;
