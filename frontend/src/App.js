6eecbe5784a980cc673d5079bf8caea052883f45
Kimberley Delgado <kimberleyvdelgado@gmail.com>
2023-07-09 21:44:19 +0800

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">TUP Business Center</Link>
+          <Navbar className="navbar-color" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>TUP Business Center</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved.</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
