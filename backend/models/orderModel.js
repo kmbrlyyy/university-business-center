2729f2c23f904add15c560b6942b5e41deeafa9c
Kimberley Delgado <kimberleyvdelgado@gmail.com>
2023-07-14 07:01:37 +0800

    created place order action

---
+++ backend/models/orderModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        slug: { type: String, required: true },
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+      },
+    ],
+    customerInfo: {
+      fullName: { type: String, required: true },
+      contactNumber: { type: String, required: true },
+      address: { type: String, required: true },
+    },
+    paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    itemsPrice: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isReceived: { type: Boolean, default: false },
+    receivedAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
